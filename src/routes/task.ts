eec9ae4aeaf834193b882cee25104f0e7d907c9a
Ar4ys <ar4ys@pm.me>
2021-04-14 12:50:19 +0300

    Rewrite routes handler using async-handler instead of explicit try catch

--- src/routes/task.ts
+++ src/routes/task.ts
@@ -1,64 +1,45 @@
 import { Router } from 'express'
+import catchAsync from 'express-async-handler'
 import TaskService from '../services/task.js'
 
 const router = Router()
 
 router.route('/task')
-  .get(async (_req, res, next) => {
-    try {
-      const allTasks = await TaskService.getAll()
-      res.status(200).send(allTasks)
-    } catch(e) {
-      next(e)
-    }
-  })
-  .post(async (req, res, next) => {
-    try {
-      const taskFields = req.body
-      const taskId = await TaskService.create(taskFields)
-      res.status(201).send({ id: taskId })
-    } catch(e) {
-      next(e)
-    }
-  })
+  .get(catchAsync(async (_req, res) => {
+    const allTasks = await TaskService.getAll()
+    res.status(200).send(allTasks)
+  }))
+  .post(catchAsync(async (req, res) => {
+    const taskFields = req.body
+    const taskId = await TaskService.create(taskFields)
+    res.status(201).send({ id: taskId })
+  }))
 
 router.route('/task/:id')
-  .get(async (req, res, next) => {
-    try {
-      const id = +req.params.id
-      const task = await TaskService.get(id)
-      if (task)
-        res.status(200).send(task)
-      else
-        res.sendStatus(404)
-    } catch(e) {
-      next(e)
-    }
-  })
-  .patch(async (req, res, next) => {
-    try {
-      const id = +req.params.id
-      const fields = req.body
-      const [ amount ] = await TaskService.update(id, fields)
-      if (amount === 0)
-        res.sendStatus(404)
-      else
-        res.sendStatus(204)
-    } catch(e) {
-      next(e)
-    }
-  })
-  .delete(async (req, res, next) => {
-    try {
-      const id = +req.params.id
-      const amount = await TaskService.delete(id)
-      if (amount === 0)
-        res.sendStatus(404)
-      else
-        res.sendStatus(204)
-    } catch(e) {
-      next(e)
-    }
-  })
+  .get(catchAsync(async (req, res) => {
+    const id = +req.params.id
+    const task = await TaskService.get(id)
+    if (task)
+      res.status(200).send(task)
+    else
+      res.sendStatus(404)
+  }))
+  .patch(catchAsync(async (req, res) => {
+    const id = +req.params.id
+    const fields = req.body
+    const [ amount ] = await TaskService.update(id, fields)
+    if (amount === 0)
+      res.sendStatus(404)
+    else
+      res.sendStatus(204)
+  }))
+  .delete(catchAsync(async (req, res) => {
+    const id = +req.params.id
+    const amount = await TaskService.delete(id)
+    if (amount === 0)
+      res.sendStatus(404)
+    else
+      res.sendStatus(204)
+  }))
 
 export default router
